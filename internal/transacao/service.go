330930a1cdf83371cded77dc0d61313c9c19441f
Isadora Souza <isadora_msouza@hotmail.com>
2024-03-09 21:15:06 -0300

    adjust channels

--- internal/transacao/service.go
+++ internal/transacao/service.go
@@ -21,14 +21,62 @@ func NewService(r Repository) Service {
 	}
 }
 
-func (s *transacaoService) CreateTransaction(ctx context.Context, t domain.Transacao) (domain.TransacaoResponse, error) {
-	response, err := s.repository.SaveTransaction(ctx, t)
+var semaphore = make(chan struct{}, 20)
 
-	return response, err
+func (s *transacaoService) CreateTransaction(ctx context.Context, t domain.Transacao) (domain.TransacaoResponse, error) {
+	responseChan := make(chan domain.TransacaoResponse, 1)
+	errChan := make(chan error, 1)
+
+	semaphore <- struct{}{}
+
+	go func() {
+		defer func() {
+			<-semaphore
+		}()
+
+		response, err := s.repository.SaveTransaction(ctx, t)
+		if err != nil {
+			errChan <- err
+			return
+		}
+		responseChan <- response
+	}()
+
+	select {
+	case response := <-responseChan:
+		return response, nil
+	case err := <-errChan:
+		return domain.TransacaoResponse{}, err
+	case <-ctx.Done():
+		return domain.TransacaoResponse{}, ctx.Err()
+	}
 }
 
 func (s *transacaoService) GetExtrato(ctx context.Context, id int) (domain.Extrato, error) {
-	extrato, err := s.repository.GetExtrato(ctx, id)
+	extratoChan := make(chan domain.Extrato, 1)
+	errChan := make(chan error, 1)
 
-	return extrato, err
+	semaphore <- struct{}{}
+
+	go func() {
+		defer func() {
+			<-semaphore
+		}()
+
+		extrato, err := s.repository.GetExtrato(ctx, id)
+		if err != nil {
+			errChan <- err
+			return
+		}
+		extratoChan <- extrato
+	}()
+
+	select {
+	case extrato := <-extratoChan:
+		return extrato, nil
+	case err := <-errChan:
+		return domain.Extrato{}, err
+	case <-ctx.Done():
+		return domain.Extrato{}, ctx.Err()
+	}
 }
